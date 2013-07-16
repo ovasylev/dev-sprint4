56b59bbeac83f98a6cce1cbcf1bf55bc66efebe5
ovasylev <ovasylev@me.com>
2013-07-16 19:55:48 -0400

    Added Ex.11.10

--- chap11.py
+++ chap11.py
@@ -41,3 +41,36 @@ Result:
 [7, 2, 1, 1, 2]
 True
 '''
+
+#------------Ex.11.10--------------
+
+def move_letter(letter, index):
+	start_pos = ord(letter)
+	end_pos = start_pos + index
+	if end_pos < 97:
+		end_pos = 123 - (97 - end_pos)
+	elif end_pos > 122:
+		end_pos = 97 + (end_pos - 123)
+	new_letter = chr(end_pos)
+	return new_letter
+
+def rotate_word(word, index):
+	new_word = ''
+	for i in range(len(word)):
+		new_word += move_letter(word[i], index)
+	return new_word
+
+def rotate_pairs(file, index):
+	temp_pairs = dict()
+	pairs = dict()
+	fin = open(file, 'r+')
+	for line in fin:
+		word = line.strip()
+		new_word = rotate_word(word, index)
+		fo = open(file, 'r+')
+		for l in fo:
+			if new_word == l.strip():
+				pairs[word] = new_word
+	return pairs
+		
+print rotate_pairs('data.txt', 2)
